0807dfbcdaa220fefe19659f052a02963516ec61
Tobias Rosenhoff <tobias.rosenhoff@stena.com>
2017-03-08 10:45:07 +0100

    Changed path to Vim config file

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -e
 echo "Creating a symbolic link for Vim config"
 
 if ! [[ -a "${HOME}/.vimrc" ]]; then
-  ln -s ~/.vim/vimrc ~/.vimrc
+  ln -s ~/.vim/.vimrc ~/.vimrc
   echo "Done."
 else echo "Already configured.";
 
